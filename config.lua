1a021577e76a5b2bf3057429e62fcfae662fc879
hsamdew <hsamdew@gmail.com>
2017-09-20 16:47:07 +0430

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,17 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "MRCore"
+  },
+  info_text = "*MaTaDoR Helper Bot V3.1*\n\n》[MaTaDoR Helper](https://github.com/MaTaDoRTEaM/matador)\n\n*》Admins :*\n*》Founder & Developer :* [MahDiRoO](Telegram.Me/mahdiroo)\n\n*》Special thanks to :*\n》[Soheil](Telegram.Me/solidev)\n\n*》Our channel :*\n》[MATaDoRTeaM](Telegram.Me/matadorteam)\n",
+  moderation = {
+    data = "/home/createbot/cli/data/moderation.json"
+  },
+  sudo_users = {
+    sudouserid,
+	useridbotcli
+  }
+}
+return _
+end
